26e85992143609fb4cecfe56322170582fe29b0e
amysews <amyc271@gmail.com>
2018-02-10 19:34:58 +0000

    Add comment and article counts to user data

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,36 @@ const mongoose = require('mongoose');
 mongoose.Promise = Promise;
 
 const Users = require('../models/users');
+const Comments = require('../models/comments');
+const Articles = require('../models/articles');
 
 function getAllUsers (req, res, next) {
-  Users.find()
-    .then(users => res.json({ users }))
+  let users;
+  Users.find().lean()
+    .then(usersResponse => users = usersResponse)
+    .then(() => {
+      const promiseCommentsCounts = users.map(user => {
+        return Comments.count({ created_by: user.username })
+      })
+      return Promise.all(promiseCommentsCounts);
+    })
+    .then((commentsCounts) => {
+      users.forEach((user, i) => {
+        user.commentsCount = commentsCounts[i];
+      })
+    })
+    .then(() => {
+      const promiseArticlesCounts = users.map(user => {
+        return Articles.count({ created_by: user.username })
+      })
+      return Promise.all(promiseArticlesCounts);
+    })
+    .then((articlesCounts) => {
+      users.forEach((user, i) => {
+        user.articlesCount = articlesCounts[i];
+      })
+    })
+    .then(() => res.json({ users }))
     .catch(next);
 }
 
